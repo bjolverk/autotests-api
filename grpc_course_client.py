58b9e67109a77856eaba5ddcd076aa58e3f64c5c
Bjolverk <Bjolverk@gmail.com>
2025-05-18 00:58:16 +0300

    Практика работы с gRPC протоколом

---
+++ grpc_course_client.py
@@ -0,0 +1,14 @@
+import grpc
+from concurrent import futures
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = course_service_pb2_grpc.CourseServiceStub(channel)
+
+responce = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id= "api-course"))
+print(responce)
+
+
