c7c64f633fbdaf1346be34b7dc3af8baae36a995
Bjolverk <Bjolverk@gmail.com>
2025-06-15 21:02:59 +0300

    Убрал лишние импорты

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,15 +1,15 @@
 from clients.authentication.authentication_client import get_authentication_client
-from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
+from clients.authentication.authentication_schema import LoginResponseSchema
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.private_users_client import get_private_users_client
+
 from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema
 from http import HTTPStatus
 
 from tools.assertions.authentication import assert_login_response
-from tools.assertions.base import assert_status_code, assert_is_true
+from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+
 
 
 def test_login():
