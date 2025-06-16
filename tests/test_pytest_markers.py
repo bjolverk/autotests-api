9e180d864d8cf6e6227e713c230840c7af43eb76
Bjolverk <Bjolverk@gmail.com>
2025-06-16 19:38:41 +0300

    Маркировки Pytest

---
+++ tests/test_pytest_markers.py
@@ -0,0 +1,17 @@
+# import pytest
+#
+# @pytest.mark.api
+# class TestUserInterface:
+#
+#     @pytest.mark.smoke
+#     @pytest.mark.critical
+#     def test_login(self):
+#         pass
+#
+#     @pytest.mark.regression
+#     def test_forgot_password(self):
+#         pass
+#
+#     @pytest.mark.smoke
+#     def test_signup(self):
+#         pass
